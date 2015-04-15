1d4aceaa36e546a1264189ff04e0e29ab769ac23
xielingyong <2867306902@qq.com>
2015-04-15 11:07:09 +0800

    ss
    sss

---
+++ ssssss/src/com/example/ssssss/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ssssss;
+
+import com.example.testdemo.Utils.HTML5WebView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+	//private String url="http://mobile.wxb.com.cn";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		HTML5WebView hv=new HTML5WebView(MainActivity.this);
+		hv.initView();
+	}
+
+
+}
